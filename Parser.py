19f987e809cb7d4054a9feb56903a4fc58085329
Ryan Gang <ryan-gg@outlook.com>
2023-08-19 21:59:52 +0530

    Add Parser stub file.

---
+++ Parser.py
@@ -0,0 +1,30 @@
+class Parser:
+    """
+    Encapsulates access to the input code. Reads an assembly language command,
+    parses it, and provides convenient access to the command's components
+    (fields and symbols). In addition, removes all white space and comments.
+    """
+
+    def __init__(self, filepath: str) -> None:
+        pass
+
+    def hasMoreCommands(self) -> None:
+        pass
+
+    def advance(self) -> None:
+        pass
+
+    def commandType(self) -> None:
+        pass
+
+    def symbol(self) -> None:
+        pass
+
+    def dest(self) -> None:
+        pass
+
+    def comp(self) -> None:
+        pass
+
+    def jump(self) -> None:
+        pass
