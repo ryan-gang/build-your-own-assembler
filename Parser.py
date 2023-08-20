758bcedddd3ec7128da2d4914a3a9970cba399da
Ryan Gang <ryan-gg@outlook.com>
2023-08-20 21:10:45 +0530

    Add commandType() method.

--- Parser.py
+++ Parser.py
@@ -1,4 +1,6 @@
+# %%
 import re
+from typing import Any
 
 
 class Parser:
@@ -8,17 +10,22 @@ class Parser:
     (fields and symbols). In addition, removes all white space and comments.
     """
 
+    A_COMMAND = 0
+    C_COMMAND = 1
+    L_COMMAND = 2
+
     def __init__(self, filepath: str) -> None:
         """
         Opens the input file/stream, reads in the contents, removes all
         whitespaces and comments.
         """
-        self.index = 0
+        self.index = -1
         self.empty_line = r"^[\s]+?$"
         self.comment = r"\/\/.*$"
         self.white_space = r"[\s]+"
 
         self.parsed: list[str] = []
+        self.current_command = ""
 
         with open(filepath, "r") as f:
             data = f.read()
@@ -44,8 +51,14 @@ class Parser:
         self.index += 1
         self.current_command = self.parsed[self.index]
 
-    def commandType(self) -> None:
-        pass
+    def commandType(self) -> Any:
+        command = self.current_command
+        if self._is_a_command(command):
+            return Parser.A_COMMAND
+        elif self._is_c_command(command):
+            return Parser.C_COMMAND
+        elif self._is_l_command(command):
+            return Parser.L_COMMAND
 
     def symbol(self) -> None:
         pass
@@ -58,3 +71,41 @@ class Parser:
 
     def jump(self) -> None:
         pass
+
+    def _is_a_command(self, command: str) -> bool:
+        a_constant = r"\d+(\.\d+)?"
+        a_symbol = r"[0-9A-Za-z_.$:]+"
+        a_start = r"^@"
+
+        a_inst_1 = re.compile(a_start + a_constant)
+        a_inst_2 = re.compile(a_start + a_symbol)
+
+        return self._is_match(a_inst_1, command) or self._is_match(a_inst_2, command)
+
+    def _is_c_command(self, command: str) -> bool:
+        token1 = r"[ADM]{0,1}"
+        operation = r"[-+!|&]{0,1}"
+        token2 = r"[ADM10]"
+        jump = r"JGT|JEQ|JGE|JLT|JNE|JLE|JMP"
+
+        # dest=comp
+        c_dest_comp = re.compile(token1 + "=" + token1 + operation + token2)
+        # comp
+        c_comp = re.compile(token1 + operation + token2)
+        # comp;jump
+        c_comp_jump = re.compile(token1 + operation + token2 + ";" + jump)
+        return (
+            self._is_match(c_comp, command)
+            or self._is_match(c_comp_jump, command)
+            or self._is_match(c_dest_comp, command)
+        )
+
+    def _is_l_command(self, command: str) -> bool:
+        a_symbol = r"[0-9A-Za-z_.$:]+"
+
+        l_inst = re.compile("(" + a_symbol + ")")
+
+        return self._is_match(l_inst, command)
+
+    def _is_match(self, pattern: re.Pattern[str], string: str) -> bool:
+        return re.match(pattern, string) is not None
