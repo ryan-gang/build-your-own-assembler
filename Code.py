8ebfdbf89e1276e5b490ef1fc909ad2360dccf0b
Ryan Gang <ryan-gg@outlook.com>
2023-08-20 22:57:42 +0530

    Add jump() method.

--- Code.py
+++ Code.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Code:
     """
     Translates Hack assembly language mnemonics into binary codes.
@@ -13,15 +16,28 @@ class Code:
         "AD": 6,
         "ADM": 7,
     }
+    jmp_symbol_table = {
+        None: 0,
+        "JGT": 1,
+        "JEQ": 2,
+        "JGE": 3,
+        "JLT": 4,
+        "JNE": 5,
+        "JLE": 6,
+        "JMP": 7,
+    }
 
-    def dest(self, mnemonic: str) -> str:
+    def dest(self, mnemonic: Optional[str]) -> str:
         mnemonic = self._sort_string(mnemonic)
         return self._to_binary(Code.dest_symbol_table[mnemonic])
+
+    def jump(self, mnemonic: Optional[str]) -> str:
+        return self._to_binary(Code.jmp_symbol_table[mnemonic])
 
     def _to_binary(self, integer: int) -> str:
         return bin(integer).split("b")[1]
 
-    def _sort_string(self, string: str) -> str:
+    def _sort_string(self, string: Optional[str]) -> Optional[str]:
         if not string:
             return string
         else:
