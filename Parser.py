afb4209798eea077a09b73716060c691ed2cc0da
Ryan Gang <ryan-gg@outlook.com>
2023-08-21 13:01:59 +0530

    Refactor out regex patterns, into common place.

--- Parser.py
+++ Parser.py
@@ -6,12 +6,40 @@ class Parser:
     """
     Encapsulates access to the input code. Reads an assembly language command,
     parses it, and provides convenient access to the command's components
-    (fields and symbols). In addition, removes all white space and comments.
+    (fields and symbols). In addition, removes all whitespaces and comments.
     """
 
     A_COMMAND = 0
     C_COMMAND = 1
     L_COMMAND = 2
+
+    # Regex strings
+    # A-Instruction
+    # @ + a_start + a_symbol | a_constant
+    # @ + a_start + a_constant -> Type 1
+    # @ + a_start + a_symbol -> Type 2
+    a_ins_cons = r"\d+(\.\d+)?"
+    a_ins_sym = r"[0-9A-Za-z_.$:]+"
+    a_ins_start = r"^@"
+    a_ins_t1 = re.compile(a_ins_start + a_ins_cons)  # A-Instruction type 1
+    a_ins_t2 = re.compile(a_ins_start + a_ins_sym)  # A-Instruction type 2
+
+    # C-Instruction
+    # c_ins_tok0 + = + c_ins_tok1 + c_ins_op + c_ins_tok2 + ; + c_ins_jmp
+    c_ins_tok0 = r"[ADM10]{0,3}"
+    c_ins_tok1 = r"[ADM10]{0,1}"
+    c_ins_op = r"[-+!|&]{0,1}"
+    c_ins_tok2 = r"[ADM10]{1}"
+    c_ins_jmp = r"(JGT|JEQ|JGE|JLT|JNE|JLE|JMP)"
+    # dest=comp
+    c_ins_dest_comp = re.compile(c_ins_tok0 + "=" + c_ins_tok1 + c_ins_op + c_ins_tok2)
+    # comp
+    c_ins_comp = re.compile(c_ins_tok1 + c_ins_op + c_ins_tok2)
+    # comp;jump
+    c_ins_comp_jump = re.compile(c_ins_tok1 + c_ins_op + c_ins_tok2 + ";" + c_ins_jmp)
+
+    # L-Instruction
+    l_ins = re.compile(r"\(" + a_ins_sym + r"\)")
 
     def __init__(self, filepath: str) -> None:
         """
@@ -39,7 +67,7 @@ class Parser:
 
     def hasMoreCommands(self) -> bool:
         """
-        Returns a boolean, denoting if there more commands in the parsed input.
+        Returns a boolean, denoting if there are more commands in the parsed input.
         """
         return self.index < len(self.parsed) - 1
 
@@ -52,7 +80,7 @@ class Parser:
 
     def commandType(self) -> Any:
         """
-        Returns the type of the current command : A_COMMAND (0), C_COMMAND (1), L_COMMAND (2).
+        Returns the type of the current command: A_COMMAND (0), C_COMMAND (1), L_COMMAND (2).
         """
         command = self.current_command
         if self._is_a_command(command):
@@ -67,26 +95,17 @@ class Parser:
         Returns the symbol or decimal Xxx of the current command @Xxx or (Xxx).
         """
         command = self.current_command
-
-        symbol = r"[0-9A-Za-z_.$:]+"
-        al_symbol = re.compile(symbol)
-
-        return self._match_result(al_symbol, command)
+        a_l_ins_symbol = re.compile(self.a_ins_sym)
+        return self._match_result(a_l_ins_symbol, command)
 
     def dest(self) -> Optional[str]:
         """
         Returns the dest mnemonic in the current C-command.
         """
         command = self.current_command
-
-        token1 = r"[ADM10]{0,1}"
-        operation = r"[-+!|&]{0,1}"
-        token2 = r"[ADM10]"
-        destination = re.compile(token1)
-
+        destination = re.compile(self.c_ins_tok0)
         # dest=comp
-        c_dest_comp = re.compile(token1 + "=" + token1 + operation + token2)
-        if self._is_match(c_dest_comp, command):
+        if self._is_match(self.c_ins_dest_comp, command):
             return self._match_result(destination, command.split("=")[0])
 
     def comp(self) -> Optional[str]:
@@ -94,37 +113,24 @@ class Parser:
         Returns the comp mnemonic in the current C-command.
         """
         command = self.current_command
-
-        token1 = r"[ADM10]{0,1}"
-        operation = r"[-+!|&]{0,1}"
-        token2 = r"[ADM10]"
-        jump = r"(JGT|JEQ|JGE|JLT|JNE|JLE|JMP)"
-        computation = re.compile(token1 + operation + token2)
-
         # dest=comp
-        c_dest_comp = re.compile(token1 + "=" + token1 + operation + token2)
-        if self._is_match(c_dest_comp, command):
-            return self._match_result(computation, command.split("=")[1])
+        if self._is_match(self.c_ins_dest_comp, command):
+            return self._match_result(self.c_ins_comp, command.split("=")[1])
 
         # comp
-        c_comp = re.compile(token1 + operation + token2)
-        if self._is_match(c_comp, command):
-            return self._match_result(computation, command)
+        if self._is_match(self.c_ins_comp, command):
+            return self._match_result(self.c_ins_comp, command)
 
         # comp;jump
-        c_comp_jump = re.compile(token1 + operation + token2 + ";" + jump)
-        if self._is_match(c_comp_jump, command):
-            return self._match_result(computation, command)
+        if self._is_match(self.c_ins_comp_jump, command):
+            return self._match_result(self.c_ins_comp, command)
 
     def jump(self) -> Optional[str]:
         """
         Returns the jump mnemonic in the current C-command.
         """
         command = self.current_command
-
-        jump = r"(JGT|JEQ|JGE|JLT|JNE|JLE|JMP)"
-        c_jump = re.compile(jump)
-
+        c_jump = re.compile(self.c_ins_jmp)
         # comp;jump
         return self._match_result(c_jump, command)
 
@@ -132,45 +138,23 @@ class Parser:
         """
         Return a bool denoting if command is an A command.
         """
-        a_constant = r"\d+(\.\d+)?"
-        a_symbol = r"[0-9A-Za-z_.$:]+"
-        a_start = r"^@"
-
-        a_inst_1 = re.compile(a_start + a_constant)
-        a_inst_2 = re.compile(a_start + a_symbol)
-
-        return self._is_match(a_inst_1, command) or self._is_match(a_inst_2, command)
+        return self._is_match(self.a_ins_t1, command) or self._is_match(self.a_ins_t2, command)
 
     def _is_c_command(self, command: str) -> bool:
         """
         Return a bool denoting if command is a C command.
         """
-        token1 = r"[ADM10]{0,1}"
-        operation = r"[-+!|&]{0,1}"
-        token2 = r"[ADM10]"
-        jump = r"(JGT|JEQ|JGE|JLT|JNE|JLE|JMP)"
-
-        # dest=comp
-        c_dest_comp = re.compile(token1 + "=" + token1 + operation + token2)
-        # comp
-        c_comp = re.compile(token1 + operation + token2)
-        # comp;jump
-        c_comp_jump = re.compile(token1 + operation + token2 + ";" + jump)
         return (
-            self._is_match(c_comp, command)
-            or self._is_match(c_comp_jump, command)
-            or self._is_match(c_dest_comp, command)
+            self._is_match(self.c_ins_comp, command)
+            or self._is_match(self.c_ins_comp_jump, command)
+            or self._is_match(self.c_ins_dest_comp, command)
         )
 
     def _is_l_command(self, command: str) -> bool:
         """
-        Return a bool denoting if command is a L command.
+        Return a bool denoting if command is an L-command.
         """
-        a_symbol = r"[0-9A-Za-z_.$:]+"
-
-        l_inst = re.compile(r"\(" + a_symbol + r"\)")
-
-        return self._is_match(l_inst, command)
+        return self._is_match(self.l_ins, command)
 
     def _is_match(self, pattern: re.Pattern[str], string: str) -> bool:
         return re.fullmatch(pattern, string) is not None
