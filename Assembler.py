0cf7ac9893572e21533ea6daf9ce966153421093
Ryan Gang <ryan-gg@outlook.com>
2023-08-21 22:14:11 +0530

    Change all params to object instance.

--- Assembler.py
+++ Assembler.py
@@ -1,5 +1,5 @@
-from Parser import Parser
 from Code import Code
+from Parser import Parser
 from SymbolTable import SymbolTable
 
 
@@ -10,38 +10,46 @@ class Assembler:
         self.c = Code()
         self.st = SymbolTable()
 
-    def first_pass(self, p: Parser, st: SymbolTable) -> None:
-        while p.has_more_commands():
-            p.advance()
-            if p.command_type() == 2:  # L
-                symbol = p.symbol()
-                c_line = p.index - p.l_ins_count
-                st.add_entry(symbol, c_line)
-                p.l_ins_count += 1
+    def first_pass(self) -> None:
+        while self.p.has_more_commands():
+            self.p.advance()
+            if self.p.command_type() == 2:  # L
+                symbol = self.p.symbol()
+                c_line = self.p.index - self.p.l_ins_count
+                self.st.add_entry(symbol, c_line)
+                self.p.l_ins_count += 1
 
-        p.__init_vars__()  # Reset variables for 2nd pass.
+        self.p.__init_vars__()  # Reset variables for 2nd pass.
 
-    def second_pass(self, p: Parser, st: SymbolTable, c: Code) -> None:
-        while p.has_more_commands():
-            p.advance()
-            c_type = p.command_type()
+    def second_pass(self) -> None:
+        while self.p.has_more_commands():
+            self.p.advance()
+            c_type = self.p.command_type()
             if c_type == 0:  # A
-                symbol = p.symbol()
-                address = self.get_address(symbol, p, st, c)
-                ins = c.a_instruction_binary(address)
+                symbol = self.p.symbol()
+                address = self.get_address(symbol)
+                ins = self.c.a_instruction_binary(address)
             elif c_type == 1:  # C
-                ins = c.c_instruction_binary(c.comp(p.comp()), c.dest(p.dest()), c.jump(p.jump()))
+                ins = self.c.c_instruction_binary(
+                    self.c.comp(self.p.comp()),
+                    self.c.dest(self.p.dest()),
+                    self.c.jump(self.p.jump()),
+                )
             else:  # L
                 continue
             print(ins)
 
-    def get_address(self, symbol: str, p: Parser, st: SymbolTable, c: Code) -> int | str:
-        if p.a_command_type() == 1:  # Constant
+    def get_address(self, symbol: str) -> int | str:
+        if self.p.a_command_type() == 1:  # Constant
             return symbol
         else:
-            if st.contains(symbol):
-                return st.get_address(symbol)
+            if self.st.contains(symbol):
+                return self.st.get_address(symbol)
             else:
-                st.add_entry(symbol, p.mem_loc)
-                p.mem_loc += 1
-                return p.mem_loc - 1
+                self.st.add_entry(symbol, self.p.mem_loc)
+                self.p.mem_loc += 1
+                return self.p.mem_loc - 1
+
+    def assemble(self):
+        self.first_pass()
+        self.second_pass()
