9213e69bfffe3221fddcbd7d5665574f48bd7ffa
Ryan Gang <ryan-gg@outlook.com>
2023-08-21 20:40:12 +0530

    Add predefined values.

--- SymbolTable.py
+++ SymbolTable.py
@@ -4,11 +4,41 @@ class SymbolTable:
     addresses (RAM and ROM).
     """
 
+    predefined = {
+        "SP": 0,
+        "LCL": 1,
+        "ARG": 2,
+        "THIS": 3,
+        "THAT": 4,
+        "R0": 0,
+        "R1": 1,
+        "R2": 2,
+        "R3": 3,
+        "R4": 4,
+        "R5": 5,
+        "R6": 6,
+        "R7": 7,
+        "R8": 8,
+        "R9": 9,
+        "R10": 10,
+        "R11": 11,
+        "R12": 12,
+        "R13": 13,
+        "R14": 14,
+        "R15": 15,
+        "SCREEN": 16384,
+        "KBD": 24576,
+    }
+
     def __init__(self) -> None:
         """
         Initializes a new empty symbol table, and adds predefined values.
         """
-        pass
+        self.symbol_table: dict[str, int] = {}
+        self._add_predefined_values()
+
+    def _add_predefined_values(self) -> None:
+        self.symbol_table = {**self.symbol_table, **self.predefined}
 
     def add_entry(self, symbol: str, address: int) -> None:
         """
